80c4e8ce610a48583f1c23cea9d789343ee7683c
MusaOglanov <oglanovmusa2001@gmail.com>
2023-03-29 23:03:29 +0400

    HotelsConroller-in index-ni yazdım

---
+++ TraveAgency/TraveAgency/Controllers/HotelsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TraveAgency.DAL;
+using TraveAgency.Models;
+
+namespace TraveAgency.Controllers
+{
+    public class HotelsController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly IWebHostEnvironment _env;
+        public HotelsController(AppDbContext db, IWebHostEnvironment env)
+        {
+            _db = db;
+            _env = env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Hotel> hotels = await _db.Hotels
+                .Include(h=>h.HotelDetail)
+                .Include(h=>h.HotelImages)
+                .Include(h=>h.HotelRoomTypes)
+                .Include(h=>h.HotelHotelCategories)
+                .ThenInclude(h=>h.HotelCategory)
+                .ToListAsync();
+            return View(hotels);
+        }
+    }
+}
