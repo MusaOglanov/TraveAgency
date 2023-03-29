d32b89cae3fca2072d2434bfda1270539b6d068c
MusaOglanov <oglanovmusa2001@gmail.com>
2023-03-29 13:37:49 +0400

    TraveAgency-ə Sql qoşdum

---
+++ TraveAgency/TraveAgency/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TraveAgency.DAL
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
