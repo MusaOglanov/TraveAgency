bfaf3333d1e618ca7584547dd6490a7878e21987
MusaOglanov <oglanovmusa2001@gmail.com>
2023-04-21 02:27:39 +0400

    Tour-un Update-ni yazdım

--- TraveAgency/TraveAgency/Controllers/ToursController.cs
+++ TraveAgency/TraveAgency/Controllers/ToursController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,7 +16,7 @@ namespace TraveAgency.Controllers
     {
         private readonly AppDbContext _db;
         private readonly IWebHostEnvironment _env;
-        public ToursController(AppDbContext db,IWebHostEnvironment env)
+        public ToursController(AppDbContext db, IWebHostEnvironment env)
         {
             _db = db;
             _env = env;
@@ -23,9 +24,9 @@ namespace TraveAgency.Controllers
         public async Task<IActionResult> Index()
         {
             List<Tour> tours = await _db.Tours
-                .Include(t=>t.TourCategory)
-                .Include(t=>t.TourHotels)
-                .ThenInclude(t=>t.Hotel)
+                .Include(t => t.TourCategory)
+                .Include(t => t.TourHotels)
+                .ThenInclude(t => t.Hotel)
                 .ToListAsync();
             return View(tours);
         }
@@ -35,8 +36,8 @@ namespace TraveAgency.Controllers
         #region get
         public async Task<IActionResult> Create()
         {
-            ViewBag.TourCategory=await _db.TourCategories.ToListAsync();
-            ViewBag.TourHotel=await _db.Hotels.ToListAsync();
+            ViewBag.TourCategory = await _db.TourCategories.ToListAsync();
+            ViewBag.TourHotel = await _db.Hotels.ToListAsync();
             return View();
         }
         #endregion
@@ -48,8 +49,8 @@ namespace TraveAgency.Controllers
         {
             ViewBag.TourCategory = await _db.TourCategories.ToListAsync();
             ViewBag.TourHotel = await _db.Hotels.ToListAsync();
-            bool IsExist=await _db.Tours.AnyAsync(t=>t.Name==tour.Name);
-            if(IsExist)
+            bool IsExist = await _db.Tours.AnyAsync(t => t.Name == tour.Name);
+            if (IsExist)
             {
                 ModelState.AddModelError("Name", "Bu ad daha əvvəl istifadə olunub");
                 return View();
@@ -80,8 +81,9 @@ namespace TraveAgency.Controllers
                 };
                 tourHotels.Add(tourHotel);
             }
+            tour.TourHotels = tourHotels;
             tour.StartDate = startDate;
-            tour.EndDate= endDate;
+            tour.EndDate = endDate;
             tour.TourCategoryId = tourCatsId;
             await _db.Tours.AddAsync(tour);
             await _db.SaveChangesAsync();
@@ -90,5 +92,98 @@ namespace TraveAgency.Controllers
         #endregion
 
         #endregion
+
+        #region MyRegion
+
+        #region get
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Tour dbTour = await _db.Tours
+                .Include(t => t.TourCategory)
+                .Include(t => t.TourHotels)
+                .ThenInclude(t => t.Hotel)
+                .FirstOrDefaultAsync(t => t.Id == id);
+            ViewBag.TourCategory = await _db.TourCategories.ToListAsync();
+            ViewBag.TourHotel = await _db.Hotels.ToListAsync();
+            return View(dbTour);
+        }
+        #endregion
+
+        #region post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Tour tour, int tourCatsId, int[] hotelsId, DateTime startDate, DateTime endDate)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Tour dbTour = await _db.Tours
+                .Include(t => t.TourCategory)
+                .Include(t => t.TourHotels)
+                .ThenInclude(t => t.Hotel)
+                .FirstOrDefaultAsync(t => t.Id == id);
+            ViewBag.TourCategory = await _db.TourCategories.ToListAsync();
+            ViewBag.TourHotel = await _db.Hotels.ToListAsync();
+
+            bool IsExist = await _db.Tours.AnyAsync(t => t.Name == tour.Name&&t.Id!=id);
+            if (IsExist)
+            {
+                ModelState.AddModelError("Name", "Bu ad daha əvvəl istifadə olunub");
+                return View();
+            }
+            if (tour.Photo != null)
+            {
+                if (!tour.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "Zəhmət olmasa Şəkil faylı seçin");
+                    return View();
+                }
+                if (tour.Photo.IsOlder2MB())
+                {
+                    ModelState.AddModelError("Photo", "Seçilə biləcək maxsimum şəkil ölçüsü 2MB-dır");
+                    return View();
+                }
+                string folder = Path.Combine(_env.WebRootPath, "assets", "img");
+                dbTour.Image = await tour.Photo.SaveImageAsync(folder);
+            }
+           
+
+            List<TourHotel> tourHotels = new List<TourHotel>();
+            foreach (int tourHotelId in hotelsId)
+            {
+                TourHotel tourHotel = new TourHotel
+                {
+                    HotelId = tourHotelId
+                };
+                tourHotels.Add(tourHotel);
+            }
+            dbTour.TourHotels = tourHotels;
+
+
+
+            dbTour.Name = tour.Name;
+            dbTour.Country = tour.Country;
+            dbTour.City = tour.City;
+            dbTour.TourPrice = tour.TourPrice;
+            dbTour.Adults = tour.Adults;
+            dbTour.Children = tour.Children;
+            dbTour.TourDescription = tour.TourDescription;
+            dbTour.Duration = tour.Duration;
+            dbTour.IsDomestic = tour.IsDomestic;
+            dbTour.StartDate = tour.StartDate;
+            dbTour.EndDate = tour.EndDate;
+            dbTour.TourCategoryId =tourCatsId;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #endregion
+
     }
 }
