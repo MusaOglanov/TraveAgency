51dfc5f34ae3bb930502ca049ff17d6273aba8c9
MusaOglanov <oglanovmusa2001@gmail.com>
2023-05-11 22:16:46 +0400

    AirlineTicket-də Create Və Update-də Keçmiş tarixi blokladım

--- TraveAgency/TraveAgency/Controllers/AirlineTicketsController.cs
+++ TraveAgency/TraveAgency/Controllers/AirlineTicketsController.cs
@@ -51,14 +51,13 @@ namespace TraveAgency.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(
-            AirlineTicket ticket,
-            int depAirId, int arrAirId,
-            int reAirId, int transAirId, int seatClassId, int transferPriceId, int? returnPriceId,int baggageId,
+            AirlineTicket ticket, int depAirId, int arrAirId,
+            int reAirId, int transAirId, int seatClassId,
+            int transferPriceId, int? returnPriceId,int baggageId,
             string departureDate, string departureTime,
-            string flightDuration, string arrivalDate, string arrivalTime,
-            string transferTime, string transferDate, string returnDate, string returnTime)
-
-
+            string flightDuration, string arrivalDate,
+            string arrivalTime, string transferTime, 
+            string transferDate, string returnDate, string returnTime)
         {
             ViewBag.DepartureAirport = await _db.Airports.ToListAsync();
             ViewBag.ArrivalAirport = await _db.Airports.ToListAsync();
@@ -66,17 +65,60 @@ namespace TraveAgency.Controllers
             ViewBag.ReturnAirport = await _db.Airports.ToListAsync();
             ViewBag.SeatClass = await _db.SeatClasses.ToListAsync();
 
+
+            //----------------------------DEPARTURE DATE TIME---------------------------------
+
+            #region DEPARTURE DATE TIME
+            string departureDateTimeStr = $"{departureDate} {departureTime}";
+            DateTime departureDateTime = DateTime.Parse(departureDateTimeStr);
+
+            DateTime currentDepDateTime = DateTime.Now;
+
+            if (departureDateTime < currentDepDateTime)
+            {
+                ModelState.AddModelError("DepartureDateTime", "Keçmiş tarixlər seçə bilməzsiniz");
+                return View();
+            }
+            ticket.DepartureDateTime = departureDateTime;
+            #endregion
+
+
+            //---------------------------ARRIVAL Airport------------------------------------
+
+            #region ARRIVAL DATE TIME
             if (arrAirId == depAirId)
             {
-                ModelState.AddModelError("ArrivalAirport", "Gediş və Eniş Aeroportları eyni ola bilməz");
+                ModelState.AddModelError("ArrivalAirport", "Uçuş və Eniş Aeroportları eyni ola bilməz");
                 return View();
             }
 
+            string arrivalDateTimeStr = $"{arrivalDate} {arrivalTime}";
+            DateTime arrivalDateTime = DateTime.Parse(arrivalDateTimeStr);
+            if (arrivalDateTime <= departureDateTime)
+            {
+                ModelState.AddModelError("ArrivalDateTime", "Uçuş və Eniş tarixləri eyni ola bilməz");
+                return View();
+            }
+            DateTime currentArrDateTime = DateTime.Now;
+
+            if (arrivalDateTime < currentArrDateTime)
+            {
+                ModelState.AddModelError("ArrivalDateTime", "Keçmiş tarixlər seçə bilməzsiniz");
+                return View();
+            }
+            ticket.ArrivalDateTime = arrivalDateTime;
+
+            #endregion
+
+
+            //-------------------------------TRANSFER AIRPORT------------------------------
+
+            #region TRANSFER AIRPORT
             if (ticket.AirlineTicketDetail.IsTransfer)
             {
-                if (transAirId==0)
+                if (transAirId == 0)
                 {
-                    ModelState.AddModelError("AirlineTicketDetail.TransferPrice", "Lutfen qiym'ti daxil edin");
+                    ModelState.AddModelError("AirlineTicketDetail.TransferPrice", "Lutfen qiyməti daxil edin");
                     return View();
                 }
                 if (transAirId == depAirId || transAirId == arrAirId)
@@ -87,12 +129,21 @@ namespace TraveAgency.Controllers
 
                 string transferDateTimeStr = $"{transferDate} {transferTime}";
                 DateTime trasferDateTime = DateTime.Parse(transferDateTimeStr);
+                if (trasferDateTime <= departureDateTime|| trasferDateTime <=arrivalDateTime)
+                {
+                    ModelState.AddModelError("TransferDateTime", "Transfer tarixi Uçuş və ya Eniş tarixləri ilə eynilik təşkil edir");
+                    return View();
+                }
                 ticket.AirlineTicketDetail.TransferTime = trasferDateTime;
                 ticket.AirlineTicketDetail.TransferPrice = transferPriceId;
                 ticket.TransferAirportId = transAirId;
-
+                
             }
+            #endregion
 
+            //---------------------------------RETURN AIRPORT--------------------------------
+            
+            #region RETUN AIRPORT
             if (ticket.AirlineTicketDetail.IsReturn)
             {
 
@@ -101,19 +152,38 @@ namespace TraveAgency.Controllers
                     ModelState.AddModelError("AirlineTicketDetail.ReturnPrice", "Qiyməti daxil edin");
                     return View();
                 }
-
+                
                 if (reAirId == depAirId || reAirId == transAirId)
                 {
                     ModelState.AddModelError("TransferAirport", "Aeroportlar eyni ola bilməz");
                     return View();
                 }
                 ticket.AirlineTicketDetail.ReturnPrice = returnPriceId;
+                
                 string returnDateTimeStr = $"{returnDate} {returnTime}";
                 DateTime returnDateTime = DateTime.Parse(returnDateTimeStr);
+                if (returnDateTime <= departureDateTime || returnDateTime <= arrivalDateTime)
+                {
+                    ModelState.AddModelError("TransferDateTime", "Dönüş tarixi Uçuş və ya Eniş tarixləri ilə eynilik təşkil edir");
+                    return View();
+                }
+                // KEÇMİŞ TARİXLƏRİ BLOKLAMAQ
+
+                DateTime currentReDateTime = DateTime.Now;
+                if (returnDateTime < currentReDateTime)
+                {
+                    ModelState.AddModelError("ReturnDateTime", "Keçmiş tarixlər seçə bilməzsiniz");
+                    return View();
+                }
                 ticket.AirlineTicketDetail.ReturnTime = returnDateTime;
                 ticket.ReturnAirportId = reAirId;
             }
+            
+            #endregion
 
+            //-------------------------------BAGGAGE-----------------------------------------
+
+            #region BAGGAGE
             if (ticket.AirlineTicketDetail.HassBaggage)
             {
                 if (baggageId == 0)
@@ -121,23 +191,20 @@ namespace TraveAgency.Controllers
                     ModelState.AddModelError("AirlineTicketDetail.BaggagePrice", "Lutfen qiym'ti daxil edin");
                     return View();
                 }
-                
+
                 ticket.AirlineTicketDetail.BaggagePrice = baggageId;
 
             }
-
-            string departureDateTimeStr = $"{departureDate} {departureTime}";
-            string arrivalDateTimeStr = $"{arrivalDate} {arrivalTime}";
+            #endregion
 
 
-            DateTime departureDateTime = DateTime.Parse(departureDateTimeStr);
-            ticket.DepartureDateTime = departureDateTime;
+            //---------------------------FLIGHT DURATION--------------------------------------
 
-            DateTime arrivalDateTime = DateTime.Parse(arrivalDateTimeStr);
-            ticket.ArrivalDateTime = arrivalDateTime;
-
+            #region FLIGHT DURATION
             TimeSpan flightDurationTime = TimeSpan.Parse(flightDuration);
             ticket.FlightDuration = flightDurationTime;
+
+            #endregion
 
 
             ticket.DepartureAirportId = depAirId;
@@ -219,13 +286,51 @@ namespace TraveAgency.Controllers
             ViewBag.ReturnAirport = await _db.Airports.ToListAsync();
             ViewBag.SeatClass = await _db.SeatClasses.ToListAsync();
 
+            //----------------------------DEPARTURE DATE TIME---------------------------------
 
+            #region DEPARTURE DATE TIME
+            string departureDateTimeStr = $"{departureDate} {departureTime}";
+            DateTime departureDateTime = DateTime.Parse(departureDateTimeStr);
+            DateTime currentDepDateTime = DateTime.Now;
+
+            if (departureDateTime < currentDepDateTime)
+            {
+                ModelState.AddModelError("DepartureDateTime", "Keçmiş tarixlər seçə bilməzsiniz");
+                return View();
+            }
+            dbTicket.DepartureDateTime = departureDateTime;
+            #endregion
+
+
+            //---------------------------ARRIVAL Airport------------------------------------
+
+            #region ARRIVAL Airport
             if (arrAirId == depAirId)
             {
                 ModelState.AddModelError("ArrivalAirport", "Gediş və Eniş Aeroportları eyni ola bilməz");
                 return View();
             }
+            string arrivalDateTimeStr = $"{arrivalDate} {arrivalTime}";
+            DateTime arrivalDateTime = DateTime.Parse(arrivalDateTimeStr);
+            if (arrivalDateTime <= departureDateTime)
+            {
+                ModelState.AddModelError("ArrivalDateTime", "Uçuş və Eniş tarixləri eyni ola bilməz");
+                return View();
+            }
+            DateTime currentArrDateTime = DateTime.Now;
 
+            if (arrivalDateTime < currentArrDateTime)
+            {
+                ModelState.AddModelError("ArrivalDateTime", "Keçmiş tarixlər seçə bilməzsiniz");
+                return View();
+            }
+            dbTicket.ArrivalDateTime = arrivalDateTime;
+
+            #endregion
+
+            //-------------------------------TRANSFER AIRPORT------------------------------
+
+            #region TRANSFER AIRPORT
             if (ticket.AirlineTicketDetail.IsTransfer)
             {
                 if (transAirId == 0)
@@ -241,12 +346,21 @@ namespace TraveAgency.Controllers
 
                 string transferDateTimeStr = $"{transferDate} {transferTime}";
                 DateTime trasferDateTime = DateTime.Parse(transferDateTimeStr);
+                if (trasferDateTime <= departureDateTime || trasferDateTime <= arrivalDateTime)
+                {
+                    ModelState.AddModelError("TransferDateTime", "Transfer tarixi Uçuş və ya Eniş tarixləri ilə eynilik təşkil edir");
+                    return View();
+                }
                 dbTicket.AirlineTicketDetail.TransferTime = trasferDateTime;
                 dbTicket.AirlineTicketDetail.TransferPrice = transferPriceId;
                 dbTicket.TransferAirportId = transAirId;
 
             }
+            #endregion
 
+            //---------------------------------RETURN AIRPORT--------------------------------
+
+            #region RETUN AIRPORT
             if (ticket.AirlineTicketDetail.IsReturn)
             {
 
@@ -264,33 +378,34 @@ namespace TraveAgency.Controllers
                 dbTicket.AirlineTicketDetail.ReturnPrice = returnPriceId;
                 string returnDateTimeStr = $"{returnDate} {returnTime}";
                 DateTime returnDateTime = DateTime.Parse(returnDateTimeStr);
+                DateTime currentReDateTime = DateTime.Now;
+                if (returnDateTime < currentReDateTime)
+                {
+                    ModelState.AddModelError("ReturnDateTime", "Keçmiş tarixlər seçə bilməzsiniz");
+                    return View();
+                }
                 dbTicket.AirlineTicketDetail.ReturnTime = returnDateTime;
                 dbTicket.ReturnAirportId = reAirId;
             }
+            #endregion
 
+            //-------------------------------BAGGAGE-----------------------------------------
+
+            #region BAGGAGE
             if (ticket.AirlineTicketDetail.HassBaggage)
             {
                 if (baggageId == 0)
                 {
-                    ModelState.AddModelError("AirlineTicketDetail.BaggagePrice", "Lutfen qiym'ti daxil edin");
+                    ModelState.AddModelError("AirlineTicketDetail.BaggagePrice", "Lutfen qiyməti daxil edin");
                     return View();
                 }
 
                 dbTicket.AirlineTicketDetail.BaggagePrice = baggageId;
 
             }
+            #endregion
 
-            string departureDateTimeStr = $"{departureDate} {departureTime}";
-            string arrivalDateTimeStr = $"{arrivalDate} {arrivalTime}";
-
-
-            DateTime departureDateTime = DateTime.Parse(departureDateTimeStr);
-            dbTicket.DepartureDateTime = departureDateTime;
-
-            DateTime arrivalDateTime = DateTime.Parse(arrivalDateTimeStr);
-            ticket.ArrivalDateTime = arrivalDateTime;
-
-            TimeSpan flightDurationTime = TimeSpan.Parse(flightDuration);
+            TimeSpan flightDurationTime =TimeSpan.Parse( flightDuration);
             dbTicket.FlightDuration = flightDurationTime;
 
 
