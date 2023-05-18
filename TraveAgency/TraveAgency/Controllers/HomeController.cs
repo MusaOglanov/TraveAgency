38ac1fc780e43c00ba01731027bbc7a1af4dee8a
MusaOglanov <oglanovmusa2001@gmail.com>
2023-05-18 18:14:00 +0400

    HomeController-in İndex Action-a Services kimi Tour,Hotel və AirlineTicket-i gətirdim

--- TraveAgency/TraveAgency/Controllers/HomeController.cs
+++ TraveAgency/TraveAgency/Controllers/HomeController.cs
@@ -1,29 +1,43 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TraveAgency.DAL;
 using TraveAgency.Models;
+using TraveAgency.ViewModels;
 
 namespace TraveAgency.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-       
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
         {
-            return View();
+            _db = db;
         }
+        public async Task<IActionResult> Index()
+        {
+            HomeVM homeVM = new HomeVM
+            {
+                Tour = await _db.Tours.FirstOrDefaultAsync(),
 
-     
+                Hotel = await _db.Hotels
+               .Include(h => h.HotelDetail)
+                .Include(h => h.HotelImages).FirstOrDefaultAsync(),
 
+                AirlineTicket = await _db.AirlineTickets
+                   .Include(t => t.DepartureAirport)
+                   .FirstOrDefaultAsync(),
 
-
-
+            };
+            return View(homeVM);
+        }
 
 
 
---
+++ TraveAgency/TraveAgency/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TraveAgency.Models;
+
+namespace TraveAgency.ViewModels
+{
+    public class HomeVM
+    {
+        public Tour Tour { get; set; }
+        public AirlineTicket AirlineTicket { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
